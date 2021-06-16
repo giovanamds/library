1edbdf446309cdb899de8aa5fcd5b9391c85b2ed
giovanamds <giovana.silva247@etec.sp.gov.br>
2021-06-16 16:43:32 -0300

    Commit projeto - 16.06.2021

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,10 +1,15 @@
 <?php
 
     // Declarar as variáveis necessárias para gerar a minha conexão com o banco de dados
-    $hostname = "fdb29.awardspace.net";
-    $dbname = "3800487_library";
-    $username = "3800487_library";
-    $password = "Et3cL1br@ry";
+    //$hostname = "fdb29.awardspace.net";
+    //$dbname = "3800487_library";
+    //$username = "3800487_library";
+    //$password = "Et3cL1br@ry";
+
+    $hostname = "sql100.epizy.com";
+    $dbname = "epiz_28902281_library";
+    $username = "epiz_28902281";
+    $password = "Ppf4XzN6uuf";
 
     try{
         $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
