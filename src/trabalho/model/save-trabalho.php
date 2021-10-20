2c46b187575afeca828f756ee8f6709e203efe1d
giovanamds <giovana.silva247@etec.sp.gov.br>
2021-10-20 14:24:42 -0300

    Commit projeto - 20.10.2021

--- src/trabalho/model/save-trabalho.php
+++ src/trabalho/model/save-trabalho.php
@@ -69,6 +69,23 @@
                                 ':f' => utf8_decode($requestData['COORIENTADOR']),
                                 ':g' => $novoNome
                             ));
+                            // Início da busca dos último cadastro efetivado
+                            $sql = $pdo->query("SELECT * FROM TRABALHO ORDER BY IDTRABALHO DESC LIMIT 1");
+
+                            while ($resultado = $sql->fetch(PDO::FETCH_ASSOC)){
+                                $IDTRABALHO = $resultado['IDTRABALHO'];
+                            }
+
+                            $indice = count(array_filter($requestData['AUTOR']));
+
+                            for($i=0; $i < $indice; $i++){
+                                $stmt = $pdo->prepare('INSERT INTO AUTOR (TRABALHO_IDTRABALHO, USUARIO_IDUSUARIO) VALUES (:a, :b)');
+                                $stmt->execute(array(
+                                    ':a' => $IDTRABALHO,
+                                    ':b' => $requestData['AUTOR'][$i]
+                                ));
+                            }
+
                             $retorno = array(
                                 "tipo" => 'success',
                                 "mensagem" => 'Trabalho cadastrado com sucesso.'
