83038cb2f934020772674f87f41eea28c0d029b7
giovanamds <giovana.silva247@etec.sp.gov.br>
2021-10-27 11:43:57 -0300

    Commit projeto - 27.10.2021

--- src/trabalho/model/save-trabalho.php
+++ src/trabalho/model/save-trabalho.php
@@ -77,7 +77,7 @@
                                 $IDTRABALHO = $resultado['IDTRABALHO'];
                             }
 
-                            $indice = count(array_filter($requestData['AUTOR']));
+                            $indice = count(array_filter($requestData['USUARIO_IDUSUARIO']));
                             for($i=0; $i<$indice; $i++){
                                 $stmt = $pdo -> prepare('INSERT INTO AUTOR (TRABALHO_IDTRABALHO, USUARIO_IDUSUARIO) VALUES (:a, :b)');
                                 $stmt -> execute(array(
